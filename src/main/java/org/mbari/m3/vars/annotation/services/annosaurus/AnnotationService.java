b5e22c09a5e665c20055f152f18884f3deda16b4
Brian Schlining <bschlining@gmail.com>
2017-05-22 16:43:21 -0700

    Testing CSS skin. Starting annosaurus service interface

---
+++ src/main/java/org/mbari/m3/vars/annotation/services/annosaurus/AnnotationService.java
@@ -0,0 +1,10 @@
+package org.mbari.m3.vars.annotation.services.annosaurus;
+
+/**
+ * @author Brian Schlining
+ * @since 2017-05-22T16:17:00
+ */
+public class AnnosaurusService {
+
+
+}
---
+++ src/main/java/org/mbari/m3/vars/annotation/ui/annotable/AssociationsTableCell.java
@@ -0,0 +1,14 @@
+package org.mbari.m3.vars.annotation.ui.annotable;
+
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableCell;
+import org.mbari.m3.vars.annotation.model.Association;
+
+/**
+ * @author Brian Schlining
+ * @since 2017-05-22T16:20:00
+ */
+public class AssociationListTableCell<S> extends TableCell<S, Association> {
+
+    private ListView<Association> listView;
+}
