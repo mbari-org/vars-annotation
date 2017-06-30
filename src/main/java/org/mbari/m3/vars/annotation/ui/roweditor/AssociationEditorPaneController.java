4f63ae0041d4f1e4e6b86b871477f966958715bf
Brian Schlining <bschlining@gmail.com>
2017-06-29 17:20:44 -0700

    Added fxml for new controllers

---
+++ src/main/java/org/mbari/m3/vars/annotation/ui/roweditor/AssociationEditorPaneController.java
@@ -0,0 +1,8 @@
+package org.mbari.m3.vars.annotation.ui.roweditor;
+
+/**
+ * @author Brian Schlining
+ * @since 2017-06-29T16:25:00
+ */
+public class AssociationEditorPaneController {
+}
---
+++ src/test/java/org/mbari/m3/vars/annotation/ui/roweditor/RowEditorPaneControllerDemo.java
@@ -0,0 +1,8 @@
+package org.mbari.m3.vars.annotation.ui.roweditor;
+
+/**
+ * @author Brian Schlining
+ * @since 2017-06-29T17:14:00
+ */
+public class RowEditorPaneControllerDemo {
+}
