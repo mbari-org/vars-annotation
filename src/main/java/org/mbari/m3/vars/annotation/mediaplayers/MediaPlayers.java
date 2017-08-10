fb0cb68b6173bbc9f3eb927d97c1e6164a9cd8c2
Brian Schlining <bschlining@gmail.com>
2017-08-10 09:07:29 -0700

    Starting integration of Sharktopoda

--- src/main/java/org/mbari/m3/vars/annotation/mediaplayers/MediaPlayers.java
+++ src/main/java/org/mbari/m3/vars/annotation/mediaplayers/MediaPlayers.java
@@ -3,8 +3,12 @@ package org.mbari.m3.vars.annotation.mediaplayers;
 import javafx.util.Pair;
 import org.mbari.m3.vars.annotation.UIToolBox;
 import org.mbari.m3.vars.annotation.events.MediaChangedEvent;
+import org.mbari.m3.vars.annotation.mediaplayers.sharktopoda.Sharktopoda;
 import org.mbari.m3.vars.annotation.mediaplayers.sharktopoda.SharktopodaPaneController;
+import org.mbari.m3.vars.annotation.model.Media;
+import org.mbari.vcr4j.VideoError;
 import org.mbari.vcr4j.VideoIO;
+import org.mbari.vcr4j.VideoState;
 import org.mbari.vcr4j.decorators.SchedulerVideoIO;
 import org.mbari.vcr4j.decorators.StatusDecorator;
 import org.mbari.vcr4j.decorators.VCRSyncDecorator;
@@ -19,48 +23,54 @@ import java.net.URI;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 
 /**
  * @author Brian Schlining
  * @since 2017-08-07T10:50:00
  */
-public class MediaPlayerController {
+public class MediaPlayers {
 
     private final UIToolBox toolBox;
+    private Sharktopoda sharktopoda = new Sharktopoda();
 
-    public MediaPlayerController(UIToolBox toolBox) {
+    public MediaPlayers(UIToolBox toolBox) {
         this.toolBox = toolBox;
         toolBox.getEventBus()
                 .toObserverable()
                 .ofType(MediaChangedEvent.class)
-                .subscribe(e -> open(e.get().getUri()));
+                .subscribe(e -> open(e.get()));
     }
 
-    private void open(URI uri) {
-        String u = uri.toString();
+    private CompletableFuture<MediaPlayer<VideoState, VideoError>> open(Media media) {
+        String u = media.getUri().toString();
         if (u.startsWith("urn:tid")) {
             // handle tape
         }
         else if (u.startsWith("http")) {
             // handle file via sharktopoda
+
+            // TODO handlers should be register so that user can select his/her preferred
+            // handler in preferences
             try {
                 Pair<Integer, Integer> portNumbers = SharktopodaPaneController.getPortNumbers();
-                SharktopodaVideoIO videoIO = new SharktopodaVideoIO(UUID.randomUUID(),
-                        "localhost", portNumbers.getKey());
-                videoIO.send(new OpenCmd(uri.toURL()));
-                new StatusDecorator<>(videoIO);
-                new VCRSyncDecorator<>(videoIO, 1000, 100, 3000000);
-                new FauxTimecodeDecorator(videoIO); // Convert elapsed-time to timecode
-                VideoIO<SharktopodaState, SharktopodaError> io = new SchedulerVideoIO<>(videoIO, Executors.newCachedThreadPool());
 
 
-                // TODO finish videoIO wiring See vars SharktopodaVideoPlayer
+                // TODO finish videoIO wiring See vars Sharktopoda
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
         }
 
+    }
+
+    private void openSharktopoda(Media media) {
+        Pair<Integer, Integer> portNumbers = SharktopodaPaneController.getPortNumbers();
+        sharktopoda.open(media, portNumbers.getKey(), portNumbers.getValue())
+                .thenAccept(mediaPlayer -> {
+
+                });
     }
 }
---
+++ src/main/java/org/mbari/m3/vars/annotation/mediaplayers/sharktopoda/Sharktopoda.java
@@ -0,0 +1,64 @@
+package org.mbari.m3.vars.annotation.mediaplayers.sharktopoda;
+
+import org.mbari.m3.vars.annotation.mediaplayers.MediaPlayer;
+import org.mbari.m3.vars.annotation.model.Media;
+import org.mbari.vcr4j.VideoIO;
+import org.mbari.vcr4j.decorators.SchedulerVideoIO;
+import org.mbari.vcr4j.decorators.StatusDecorator;
+import org.mbari.vcr4j.decorators.VCRSyncDecorator;
+import org.mbari.vcr4j.sharktopoda.SharktopodaError;
+import org.mbari.vcr4j.sharktopoda.SharktopodaState;
+import org.mbari.vcr4j.sharktopoda.SharktopodaVideoIO;
+import org.mbari.vcr4j.sharktopoda.commands.OpenCmd;
+import org.mbari.vcr4j.sharktopoda.commands.SharkCommands;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author Brian Schlining
+ * @since 2017-08-10T08:15:00
+ */
+public class Sharktopoda {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final AtomicReference<MediaPlayer<SharktopodaState, SharktopodaError>> currentVideoController = new AtomicReference<>();
+
+    public boolean canOpen(Media media) {
+        return false;
+    }
+
+    public CompletableFuture<MediaPlayer<SharktopodaState, SharktopodaError>> open(Media media, int sharktopodaPort, int framecapturePort) {
+        CompletableFuture<MediaPlayer<SharktopodaState, SharktopodaError>> cf = new CompletableFuture<>();
+
+        try {
+            SharktopodaVideoIO videoIO = new SharktopodaVideoIO(UUID.randomUUID(), "localhost", sharktopodaPort);
+            videoIO.send(new OpenCmd(media.getUri().toURL()));
+            new StatusDecorator<>(videoIO);
+            new VCRSyncDecorator<>(videoIO, 1000, 100, 3000000);
+            //new FauxTimecodeDecorator(videoIO); // Convert elapsed-time to timecode
+            VideoIO<SharktopodaState, SharktopodaError> io =
+                    new SchedulerVideoIO<>(videoIO, Executors.newCachedThreadPool());
+            MediaPlayer<SharktopodaState, SharktopodaError> oldVc = currentVideoController.get();
+            if (oldVc != null) {
+                oldVc.getVideoIO().send(SharkCommands.CLOSE);
+                oldVc.getImageCaptureService().dispose();
+            }
+            MediaPlayer<SharktopodaState, SharktopodaError> newVc =
+                    new MediaPlayer<>(new SharktopodaImageCaptureService(videoIO, framecapturePort), io);
+            currentVideoController.set(newVc);
+            cf.complete(newVc);
+            io.send(SharkCommands.SHOW);
+        }
+        catch (Exception e) {
+            log.error("Failed to create SharktopodaVideoIO", e);
+            cf.completeExceptionally(e);
+        }
+
+        return cf;
+    }
+}
