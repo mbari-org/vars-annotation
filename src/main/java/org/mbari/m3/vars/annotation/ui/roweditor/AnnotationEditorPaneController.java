2a6aec54202c6d1ee055fa2aa1bbc4737c75c778
Brian Schlining <bschlining@gmail.com>
2017-10-20 14:20:13 -0700

    check point

--- src/main/java/org/mbari/m3/vars/annotation/ui/roweditor/AnnotationEditorPaneController.java
+++ src/main/java/org/mbari/m3/vars/annotation/ui/roweditor/AnnotationEditorPaneController.java
@@ -166,8 +166,6 @@ public class AnnotationEditorPaneController {
                                     });
                                 });
                             });
-
-
                 }
             }
         });
