5dbed8174c7d8241f19f1e59e644b1f9ff0eacf4
Brian Schlining <bschlining@gmail.com>
2018-08-15 13:58:30 -0700

    Testing signing w/ gpg 2

--- src/main/java/org/mbari/m3/vars/annotation/ui/ImageViewController.java
+++ src/main/java/org/mbari/m3/vars/annotation/ui/ImageViewController.java
@@ -11,7 +11,6 @@ import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Window;
 import javafx.util.StringConverter;
