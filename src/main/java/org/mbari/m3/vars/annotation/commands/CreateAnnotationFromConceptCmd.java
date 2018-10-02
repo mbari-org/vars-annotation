94299da807e6723d8ee3775c32e98cf5f4eeb50d
Brian Schlining <bschlining@gmail.com>
2018-10-02 16:33:32 -0700

    Issue #92: Resolved

--- src/main/java/org/mbari/m3/vars/annotation/commands/CreateAnnotationFromConceptCmd.java
+++ src/main/java/org/mbari/m3/vars/annotation/commands/CreateAnnotationFromConceptCmd.java
@@ -1,11 +1,27 @@
 package org.mbari.m3.vars.annotation.commands;
 
+import io.reactivex.Observable;
 import org.mbari.m3.vars.annotation.EventBus;
 import org.mbari.m3.vars.annotation.UIToolBox;
 import org.mbari.m3.vars.annotation.events.AnnotationsAddedEvent;
 import org.mbari.m3.vars.annotation.events.AnnotationsRemovedEvent;
 import org.mbari.m3.vars.annotation.events.AnnotationsSelectedEvent;
+import org.mbari.m3.vars.annotation.messages.ShowAlert;
+import org.mbari.m3.vars.annotation.messages.ShowExceptionAlert;
+import org.mbari.m3.vars.annotation.messages.ShowFatalErrorAlert;
+import org.mbari.m3.vars.annotation.messages.ShowWarningAlert;
 import org.mbari.m3.vars.annotation.model.Annotation;
+import org.mbari.m3.vars.annotation.model.Concept;
+import org.mbari.m3.vars.annotation.model.ConceptDetails;
+import org.mbari.m3.vars.annotation.util.AsyncUtils;
+import org.mbari.util.Tuple2;
+import org.mbari.vcr4j.VideoIndex;
+
+import javax.swing.text.html.Option;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.concurrent.CompletableFuture;
 
 
 /**
@@ -21,25 +37,59 @@ public class CreateAnnotationFromConceptCmd implements Command {
         this.concept = concept;
     }
 
+    private void createAnnotation(UIToolBox toolBox, String primaryConcept, VideoIndex videoIndex) {
+        Annotation a0 = CommandUtil.buildAnnotation(toolBox.getData(), primaryConcept, videoIndex);
+
+        Observable<Annotation> observable = AsyncUtils.observe(toolBox.getServices()
+                .getAnnotationService()
+                .createAnnotation(a0));
+
+        observable.filter(Objects::nonNull)
+                .subscribe(a -> {
+                    this.annotation = a;
+                    EventBus eventBus = toolBox.getEventBus();
+                    eventBus.send(new AnnotationsAddedEvent(a));
+                    eventBus.send(new AnnotationsSelectedEvent(a));
+                }, t -> sendAlertMsg(toolBox, t));
+
+    }
+
+    private void sendAlertMsg(UIToolBox toolBox, Throwable t) {
+        ResourceBundle i18n = toolBox.getI18nBundle();
+        String title = i18n.getString("commands.createannotation.title");
+        String header = i18n.getString("commands.createannotation.header");
+        String content = i18n.getString("commands.createannotation.content");
+        Exception e;
+        if (t instanceof Exception) {
+            e = (Exception) t;
+        }
+        else {
+            e = new RuntimeException(t);
+        }
+        ShowAlert msg = new ShowExceptionAlert(title, header, content, e);
+        toolBox.getEventBus().send(msg);
+    }
+
     @Override
     public void apply(UIToolBox toolBox) {
-        toolBox.getMediaPlayer()
-                .requestVideoIndex()
-                .thenAccept(videoIndex -> {
-                    Annotation a0 = CommandUtil.buildAnnotation(toolBox.getData(), concept, videoIndex);
-                    toolBox.getServices()
-                            .getAnnotationService()
-                            .createAnnotation(a0)
-                            .thenAccept(a1 -> {
-                                annotation = a1;
-                                if (a1 != null) {
-                                    EventBus eventBus = toolBox.getEventBus();
-                                    eventBus.send(new AnnotationsAddedEvent(a1));
-                                    eventBus.send(new AnnotationsSelectedEvent(a1));
-                                }
-                            });
-                });
+        Observable<VideoIndex> videoIndexObservable = AsyncUtils.observe(toolBox.getMediaPlayer()
+                .requestVideoIndex());
+        Observable<Optional<ConceptDetails>> conceptObservable = AsyncUtils.observe(toolBox.getServices()
+                .getConceptService()
+                .findDetails(concept));
+        Observable<CreateDatum> lookupObservable = Observable.combineLatest(conceptObservable,
+                videoIndexObservable,
+                CreateDatum::new);
+
+        lookupObservable.filter(cd -> cd.conceptDetails.isPresent())
+                .subscribe(cd -> createAnnotation(toolBox,
+                        cd.conceptDetails.get().getName(),
+                        cd.videoIndex),
+                        throwable -> sendAlertMsg(toolBox, throwable));
     }
+
+
+
 
     @Override
     public void unapply(UIToolBox toolBox) {
@@ -58,5 +108,15 @@ public class CreateAnnotationFromConceptCmd implements Command {
     @Override
     public String getDescription() {
         return "Create Annotation using " + concept;
+    }
+}
+
+class CreateDatum {
+    final Optional<ConceptDetails> conceptDetails;
+    final VideoIndex videoIndex;
+
+    CreateDatum(Optional<ConceptDetails> conceptDetails, VideoIndex videoIndex) {
+        this.conceptDetails = conceptDetails;
+        this.videoIndex = videoIndex;
     }
 }
