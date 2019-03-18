bca013f37c830c3e684c62c46ecb863e08505363
Brian Schlining <bschlining@gmail.com>
2019-03-18 11:12:37 -0700

    check point

--- src/main/java/org/mbari/m3/vars/annotation/ui/imageanno/BoundingBoxLayerController.java
+++ src/main/java/org/mbari/m3/vars/annotation/ui/imageanno/BoundingBoxLayerController.java
@@ -48,11 +48,6 @@ public class BoundingBoxLayerController implements LayerController {
     }
 
     @Override
-    public void select(Annotation annotation, Image image) {
-
-    }
-
-    @Override
     public ToolBar getToolBar() {
         return null;
     }
