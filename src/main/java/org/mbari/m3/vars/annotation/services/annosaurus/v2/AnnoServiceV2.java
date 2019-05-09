8c69a7e22aa082462f404a8766ec7d5c9f5deb91
Brian Schlining <bschlining@gmail.com>
2019-05-09 11:23:25 -0700

    Fixed bug in page chunking

--- src/main/java/org/mbari/m3/vars/annotation/services/annosaurus/v2/AnnoServiceV2.java
+++ src/main/java/org/mbari/m3/vars/annotation/services/annosaurus/v2/AnnoServiceV2.java
@@ -1,8 +1,10 @@
 package org.mbari.m3.vars.annotation.services.annosaurus.v2;
 
+import org.mbari.m3.vars.annotation.services.RetrofitWebService;
+
 /**
  * @author Brian Schlining
  * @since 2019-05-09T10:43:00
  */
-public class AnnoServiceV2 {
+public class AnnoServiceV2 implements RetrofitWebService {
 }
--- src/main/java/org/mbari/m3/vars/annotation/services/annosaurus/v2/AnnoWebServiceV2.java
+++ src/main/java/org/mbari/m3/vars/annotation/services/annosaurus/v2/AnnoWebServiceV2.java
@@ -1,8 +1,22 @@
 package org.mbari.m3.vars.annotation.services.annosaurus.v2;
 
+import org.mbari.m3.vars.annotation.model.Annotation;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import java.util.List;
+import java.util.UUID;
+
 /**
  * @author Brian Schlining
  * @since 2019-05-09T10:44:00
  */
-public class AnnoWebServiceV2 {
+public interface AnnoWebServiceV2 {
+
+    @GET("annotations/videoreference/{uuid}")
+    Call<List<Annotation>> findByVideoReferenceUuid(@Path("uuid") UUID uuid,
+                                                    @Query("limit") Long limit,
+                                                    @Query("offset") Long offset);
 }
