3bfa29884bec95549bb2548b4e591d856c68c35c
Brian Schlining <bschlining@gmail.com>
2019-06-04 16:04:23 -0700

    check point

--- src/main/java/org/mbari/m3/vars/annotation/services/CachedReferenceNumberService.java
+++ src/main/java/org/mbari/m3/vars/annotation/services/CachedReferenceNumberService.java
@@ -5,4 +5,6 @@ package org.mbari.m3.vars.annotation.services;
  * @since 2019-06-04T16:03:00
  */
 public class CachedReferenceNumberService {
+    //private final AnnotationService annotationService;
+
 }
