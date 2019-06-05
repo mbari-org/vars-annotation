5c83494e19a77127f11a30955fa17a1b2ed1855a
Brian Schlining <bschlining@gmail.com>
2019-06-04 21:10:32 -0700

    check point

--- src/main/java/org/mbari/m3/vars/annotation/services/CachedReferenceNumberService.java
+++ src/main/java/org/mbari/m3/vars/annotation/services/CachedReferenceNumberService.java
@@ -2,6 +2,7 @@ package org.mbari.m3.vars.annotation.services;
 
 import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.mbari.m3.vars.annotation.UIToolBox;
 import org.mbari.m3.vars.annotation.model.Association;
 import org.mbari.m3.vars.annotation.model.Media;
 import org.mbari.m3.vars.annotation.util.AsyncUtils;
@@ -18,27 +19,51 @@ import java.util.stream.Collectors;
  */
 public class CachedReferenceNumberService {
 
-    private static class NewNumberKey {
-        final Media media;
+    private static class OldNumberKey {
+        private final Media media;
+        private final String concept;
+
+        public OldNumberKey(Media media, String concept) {
+            this.media = media;
+            this.concept = concept;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            OldNumberKey that = (OldNumberKey) o;
+            return media.equals(that.media) &&
+                    concept.equals(that.concept);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(media, concept);
+        }
     }
 
+
+    private final UIToolBox toolBox;
     private final AnnotationService annotationService;
     private final MediaService mediaService;
-    private final AsyncLoadingCache<Media, List<Media>> loadedMediaCache;
+    private final List<Media> medias = new CopyOnWriteArrayList<>();
+    private final List<Association> remoteNewNumbers = new CopyOnWriteArrayList<>();
     private final AsyncLoadingCache<String, List<Association>> oldNumberCache;
-    private final List<Association> oldNumbers = new CopyOnWriteArrayList<>();
+    private final String associationKey;
 
-    public CachedReferenceNumberService(AnnotationService annotationService,
-                                        MediaService mediaService) {
-        this.annotationService = annotationService;
-        this.mediaService = mediaService;
+
+    public CachedReferenceNumberService(UIToolBox toolBox) {
+        this.toolBox = toolBox;
+        this.annotationService = toolBox.getServices().getAnnotationService();
+        this.mediaService = toolBox.getServices().getMediaService();
+        associationKey = toolBox.getConfig()
+                .getString("app.annotation.identity.reference");
 
         oldNumberCache = Caffeine.newBuilder()
-                .expireAfterAccess(Duration.ofMinutes(60));
+                .expireAfterAccess(Duration.ofMinutes(60))
+                .buildAsync((key, executor) -> {})
 
-        loadedMediaCache = Caffeine.newBuilder()
-                .expireAfterWrite(Duration.ofMinutes(120))
-                .buildAsync((key, executor) -> mediaService.findByVideoSequenceName(key.getVideoSequenceName());
     }
 
     public synchronized void clear() {
@@ -46,37 +71,78 @@ public class CachedReferenceNumberService {
     }
 
     public CompletableFuture<List<Media>> findRelatedMedia(Media media) {
-        return loadedMediaCache.get(media);
+        if (!medias.isEmpty() && !medias.contains(media)) {
+            clear();
+        }
+
+        if (medias.isEmpty()) {
+           return  mediaService.findByVideoSequenceName(media.getVideoSequenceName())
+                    .thenApply(ms -> {
+                       medias.addAll(ms);
+                       return ms;
+                    });
+        }
+        else {
+            return CompletableFuture.completedFuture(medias);
+        }
     }
 
-    private CompletableFuture<List<Association>> loadNewNumbers(Media media, String associationKey) {
+    public CompletableFuture<List<Association>> findRemoteNewNumbers(Media media) {
+        return findRelatedMedia(media)
+                .thenCompose(ms -> {
+                    if (remoteNewNumbers.isEmpty()) {
+                        return AsyncUtils.collectAll(ms, m ->
+                                annotationService.findByVideoReferenceAndLinkName(m.getVideoReferenceUuid(), associationKey))
+                            .thenApply(associationLists ->
+                                associationLists.stream()
+                                        .flatMap(List::stream)
+                                        .collect(Collectors.toList()));
+                    }
+                    else {
+                        return CompletableFuture.completedFuture(remoteNewNumbers);
+                    }
+                });
+    }
 
+    public CompletableFuture<List<Association>> findNewReferenceNumbers(Media media) {
+        return findRemoteNewNumbers(media)
+                .thenApply(associations -> {
+                    associations.addAll(currentReferences());
+                    return associations.stream()
+                            .sorted(Comparator.comparing(Association::getLinkValue))
+                            .collect(Collectors.toList());
+                });
+    }
 
+    private List<Association> currentReferences() {
+        return toolBox.getData()
+                .getAnnotations()
+                .stream()
+                .flatMap(a -> a.getAssociations().stream())
+                .filter(a -> a.getLinkName().equals(associationKey))
+                .collect(Collectors.toList());
+    }
 
-        return mediaService.findByVideoSequenceName(media.getVideoSequenceName())
-                .thenCompose(medias -> AsyncUtils.collectAll(medias, m ->
-                        annotationService.findByVideoReferenceAndLinkName(m.getVideoReferenceUuid(), associationKey)))
+    private List<Association> currentReferences(String concept) {
+        return toolBox.getData()
+                .getAnnotations()
+                .stream()
+                .filter(a -> a.getConcept().equals(concept))
+                .flatMap(a -> a.getAssociations().stream())
+                .filter(a -> a.getLinkName().equals(associationKey))
+                .collect(Collectors.toList());
+    }
+
+    private CompletableFuture<List<Association>> loadOldNumbers(Media media,
+                                                                  String concept) {
+        return findRelatedMedia(media)
+                .thenCompose(ms -> AsyncUtils.collectAll(medias,
+                        m -> annotationService.findByVideoReferenceAndLinkNameAndConcept(m.getVideoReferenceUuid(),
+                                associationKey,
+                                concept)))
                 .thenApply(associationLists ->
                      associationLists.stream()
                             .flatMap(List::stream)
                             .collect(Collectors.toList()));
-
-    }
-
-    private CompletableFuture<Map<String, String>> loadOldNumbers(Media media,
-                                                                  String associationKey,
-                                                                  String concept) {
-        mediaService.findByVideoSequenceName(media.getVideoSequenceName())
-                .thenCompose(medias -> {
-                    medias.forEach(m -> {
-                        annotationService.findByVideoReferenceAndLinkNameAndConcept(m.getVideoReferenceUuid(), associationKey, concept)
-                                .thenAccept(associations -> {
-                                    Map<String, List<Association>> map = new HashMap<>();
-                                    map.put(associationKey, )
-
-                                    oldNumberCache.synchronous().put(media, )
-                                })
-                    });
-                });
     }
 }
