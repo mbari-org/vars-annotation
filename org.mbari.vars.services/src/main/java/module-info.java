797fcd0245a68f5006bce63f053d10df741a4a27
Brian Schlining <bschlining@gmail.com>
2019-07-15 10:54:42 -0700

    updated reference.conf

--- org.mbari.vars.services/src/main/java/module-info.java
+++ org.mbari.vars.services/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.mbari.vars.services {
   requires com.github.benmanes.caffeine;
   requires com.google.common;
+  requires com.google.guice;
   requires gson.javatime.serialisers;
   requires gson;
   requires io.reactivex.rxjava2;
@@ -8,6 +9,7 @@ module org.mbari.vars.services {
   requires java.jwt;
   requires java.prefs;
   requires java.scripting;
+  requires java.sql;
   requires javax.inject;
   requires jsr305;
   requires okhttp3.logging;
@@ -17,6 +19,18 @@ module org.mbari.vars.services {
   requires slf4j.api;
   requires typesafe.config;
   requires vcr4j.core;
+
+  opens org.mbari.vars.services.gson to com.google.guice;
+  opens org.mbari.vars.services.impl.annosaurus.v1 to com.google.guice;
+  opens org.mbari.vars.services.impl.annosaurus.v2 to com.google.guice;
+  opens org.mbari.vars.services.impl.panoptes.v1 to com.google.guice;
+  opens org.mbari.vars.services.impl.vampiresquid.v1 to com.google.guice;
+  opens org.mbari.vars.services.impl.varskbserver.v1 to com.google.guice;
+  opens org.mbari.vars.services.impl.varsuserserver.v1 to com.google.guice;
+  opens org.mbari.vars.services.model to com.google.guice;
+  opens org.mbari.vars.services.util to com.google.guice;
+  opens org.mbari.vars.services to com.google.guice;
+
   exports org.mbari.vars.services.gson;
   exports org.mbari.vars.services.impl.annosaurus.v1;
   exports org.mbari.vars.services.impl.annosaurus.v2;
