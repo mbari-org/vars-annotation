0f18585f31e609a63f8a2ad69883584cbb62d75b
Brian Schlining <bschlining@gmail.com>
2019-09-19 16:50:34 -0700

    Updated location of MBARI's config vars

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # build script used internally at MBARI
 
 export JPACKAGE_HOME="$HOME/Applications/jdk-14.jdk/Contents/Home"
-source "$HOME/workspace/m3-deployspace/m3.shore.mbari.org/vars-annotation/env-config.sh"
+source "$HOME/workspace/m3-deployspace/vars-annotation/env-config.sh"
 gradlew clean jpackage
