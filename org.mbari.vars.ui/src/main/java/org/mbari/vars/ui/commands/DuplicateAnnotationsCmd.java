c85f01088defb196c649593306d4f8691e889d89
Brian Schlining <bschlining@gmail.com>
2019-10-31 15:20:14 -0700

    M3-52: Fix for copying annotations to new timecode

--- org.mbari.vars.ui/src/main/java/org/mbari/vars/ui/commands/DuplicateAnnotationsCmd.java
+++ org.mbari.vars.ui/src/main/java/org/mbari/vars/ui/commands/DuplicateAnnotationsCmd.java
@@ -10,9 +10,7 @@ import org.mbari.vars.services.model.Media;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -34,17 +32,18 @@ public class DuplicateAnnotationsCmd implements Command {
 
     public Annotation makeDuplicate(Annotation annotation, String user, String activity) {
         Annotation duplicate = new Annotation(annotation);
+
         duplicate.setObservationUuid(null);
         duplicate.setObservationTimestamp(Instant.now());
         duplicate.setObserver(user);
         duplicate.setActivity(activity);
 
+        // M3-52: Don't duplicate imaged-moments
+        duplicate.setImages(Collections.emptyList());
+
         // M3-41 - video lab doesn't want associations copied
         duplicate.setAssociations(Collections.emptyList());
 
-        // if we don't null the associations uuid, it will fail to insert due to
-        // a duplicate primary key clash.
-//        duplicate.getAssociations().forEach(Association::resetUuid);
         return duplicate;
     }
 
@@ -53,6 +52,7 @@ public class DuplicateAnnotationsCmd implements Command {
         Media media = toolBox.getData().getMedia();
         if (media.getStartTimestamp() != null ) {
             duplicateAnnotations.forEach(annotation -> {
+                annotation.setVideoReferenceUuid(media.getVideoReferenceUuid());
                 Duration elapsedTime = annotation.getElapsedTime();
                 // Calculate timestamp from media start time and annotation elapsed time
                 if (elapsedTime != null) {
@@ -67,17 +67,20 @@ public class DuplicateAnnotationsCmd implements Command {
                 .getAnnotationService()
                 .createAnnotations(duplicateAnnotations)
                 .thenAccept(annos -> {
+                    // Filter out any that already exist in the table
+                    List<Annotation> newAnnos = filterPrexisting(toolBox, annos);
                     // Replace our existing dups with the ones with the correct observation uuids
                     duplicateAnnotations.clear();
-                    duplicateAnnotations.addAll(annos);
+                    duplicateAnnotations.addAll(newAnnos);
                     toolBox.getEventBus()
-                            .send(new AnnotationsAddedEvent(annos));
+                            .send(new AnnotationsAddedEvent(newAnnos));
                     if (selectAnnotations) {
                         toolBox.getEventBus()
-                                .send(new AnnotationsSelectedEvent(annos));
+                                .send(new AnnotationsSelectedEvent(newAnnos));
                     }
                 });
     }
+
 
     @Override
     public void unapply(UIToolBox toolBox) {
@@ -91,6 +94,17 @@ public class DuplicateAnnotationsCmd implements Command {
                 .thenAccept(v ->
                    toolBox.getEventBus()
                         .send(new AnnotationsRemovedEvent(duplicateAnnotations)));
+    }
+
+    private List<Annotation> filterPrexisting(UIToolBox toolBox, Collection<Annotation> annotations) {
+        List<Annotation> existingAnnotations = new ArrayList<>(toolBox.getData().getAnnotations());
+        List<Annotation> newAnnotations = new ArrayList<>();
+        for (Annotation a: annotations) {
+            if (!existingAnnotations.contains(a)) {
+                newAnnotations.add(a);
+            }
+        }
+        return newAnnotations;
     }
 
     @Override
