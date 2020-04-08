b78de72bd32db83fe8b5eecb0d5267e6164549df
Brian Schlining <bschlining@gmail.com>
2020-04-08 13:57:55 -0700

    M3-107: Gradle build now modifies Info.plist for macOS builds

--- build.sh
+++ build.sh
@@ -6,30 +6,6 @@ export JPACKAGE_HOME="/Library/Java/JavaVirtualMachines/jdk-14.jdk/Contents/Home
 source "$HOME/workspace/m3-deployspace/vars-annotation/env-config.sh"
 gradlew clean jpackage --info
 
-cat << EOF
-Next Steps:
-
-  1. Open VARS Annotation/Contents/Info.plist in XCode
-
-  2. Add the following key/value to enable access to cameras. Note that in XCode
-    this appears as a "Privacy - ..." setting
-
-<key>NSCameraUsageDescription</key>
-<string>VARS Annotation uses camera access for framecapture from hardware</string>
-
-  3. Create a new folder, the name is not important
-
-  4. Copy "VARS Annotation.app" into it
-
-  6. Create a short cut to "Applications" and copy it into the new folder
-
-  5. Open "Disk Utility.app"
-
-  6. Select the menu: "File -> new Image -> Image from folder..."
-
-  7. Rename it to the existing DMG file (e.g. "VARS Annotation-0.3.22.dmg")
-
-EOF
 
 
 
