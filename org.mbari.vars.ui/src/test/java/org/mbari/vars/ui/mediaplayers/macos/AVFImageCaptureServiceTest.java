e0b4c8cbb6def563d22a03636446d01043048faf
Brian Schlining <bschlining@gmail.com>
2021-06-18 16:20:35 -0700

    Updated dependencies. Removed AVFoundation code

--- org.mbari.vars.ui/src/test/java/org/mbari/vars/ui/mediaplayers/macos/AVFImageCaptureServiceTest.java
+++ org.mbari.vars.ui/src/test/java/org/mbari/vars/ui/mediaplayers/macos/AVFImageCaptureServiceTest.java
@@ -3,9 +3,9 @@ package org.mbari.vars.ui.mediaplayers.macos;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.mbari.vars.avfoundation.AVFImageCapture;
-import org.mbari.vars.services.model.Framegrab;
-import org.mbari.vars.ui.mediaplayers.macos.AVFImageCaptureService;
+// import org.mbari.vars.avfoundation.AVFImageCapture;
+// import org.mbari.vars.services.model.Framegrab;
+// import org.mbari.vars.ui.mediaplayers.macos.AVFImageCaptureService;
 
 import java.awt.*;
 import java.io.IOException;
@@ -25,36 +25,36 @@ public class AVFImageCaptureServiceTest {
     @Ignore
     @Test
     public void testImageCapture() throws Exception {
-        AVFImageCapture ic = new AVFImageCapture();
-        String[] devices = ic.videoDevicesAsStrings();
-        if (devices.length > 0) {
-            ic.startSessionWithNamedDevice(devices[0]);
-            Path path = Paths.get("target",
-                    getClass().getSimpleName() + "-0-" + Instant.now() + ".png");
-            Optional<Image> png = ic.capture(path.toFile());
-            Assert.assertTrue(png.isPresent());
-            ic.stopSession();
-        }
-        else {
-            System.err.println("No frame capture devices were found");
-        }
+        // AVFImageCapture ic = new AVFImageCapture();
+        // String[] devices = ic.videoDevicesAsStrings();
+        // if (devices.length > 0) {
+        //     ic.startSessionWithNamedDevice(devices[0]);
+        //     Path path = Paths.get("target",
+        //             getClass().getSimpleName() + "-0-" + Instant.now() + ".png");
+        //     Optional<Image> png = ic.capture(path.toFile());
+        //     Assert.assertTrue(png.isPresent());
+        //     ic.stopSession();
+        // }
+        // else {
+        //     System.err.println("No frame capture devices were found");
+        // }
     }
 
     @Ignore
     @Test
     public void testImageCaptureService() throws IOException {
-        AVFImageCaptureService service = AVFImageCaptureService.getInstance();
-        Collection<String> devices = service.listDevices();
-        if (devices.size() > 0) {
-            service.setDevice(devices.iterator().next());
-            Path path = Paths.get("target",
-                    getClass().getSimpleName() + "-1-" + Instant.now() + ".png");
-            Framegrab png = service.capture(path.toFile());
-            Assert.assertTrue(png.getImage().isPresent());
-        }
-        else {
-            System.err.println("No frame capture devices were found");
-        }
+        // AVFImageCaptureService service = AVFImageCaptureService.getInstance();
+        // Collection<String> devices = service.listDevices();
+        // if (devices.size() > 0) {
+        //     service.setDevice(devices.iterator().next());
+        //     Path path = Paths.get("target",
+        //             getClass().getSimpleName() + "-1-" + Instant.now() + ".png");
+        //     Framegrab png = service.capture(path.toFile());
+        //     Assert.assertTrue(png.getImage().isPresent());
+        // }
+        // else {
+        //     System.err.println("No frame capture devices were found");
+        // }
     }
 
     @Ignore
