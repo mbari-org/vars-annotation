f2b020e7d64e68b7c65795cca20cf430799b2cc2
Brian Schlining <bschlining@gmail.com>
2021-11-16 09:07:39 -0800

    checkpoint

---
+++ org.mbari.vars.ui/src/main/java/org/mbari/vars/ui/mediaplayers/macos/avf/package-info.java
@@ -0,0 +1,7 @@
+package org.mbari.vars.ui.mediaplayers.macos.avf;
+
+/**
+ * This package contains mac specific code for accessing video capture hardware via AVFoundation.
+ * It's currently disabled due to changes in Apple's GateKeeper security. The replacement
+ * is to use libbbmagic code instead.d
+ */
