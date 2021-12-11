1ac4654907b7771b973ca52da5f34fd0bfe27940
Brian Schlining <bschlining@gmail.com>
2021-12-10 17:16:13 -0800

    CU-200r89b , #124 - AES to use SHA256

--- org.mbari.vars.core/src/main/java/org/mbari/vars/core/crypto/AES.java
+++ org.mbari.vars.core/src/main/java/org/mbari/vars/core/crypto/AES.java
@@ -22,7 +22,7 @@ public class AES {
         MessageDigest sha = null;
         try {
             key = myKey.getBytes("UTF-8");
-            sha = MessageDigest.getInstance("SHA-1");
+            sha = MessageDigest.getInstance("SHA-256");
             key = sha.digest(key);
             key = Arrays.copyOf(key, 16);
             secretKey = new SecretKeySpec(key, "AES");
