e1a08edcab9dae677adc9a349271e9e7ba6070da
Brian Schlining <bschlining@gmail.com>
2022-06-02 16:02:40 -0700

    Dialing down log level

--- org.mbari.vars.ui/src/main/java/org/mbari/vars/ui/commands/CopyAnnotationsCmd.java
+++ org.mbari.vars.ui/src/main/java/org/mbari/vars/ui/commands/CopyAnnotationsCmd.java
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
  */
 public class CopyAnnotationsCmd implements Command {
 
-    private final Collection<Annotation> copiedAnnotations;
+    private final Collection<java.lang.annotation.Annotation> copiedAnnotations;
 
     public CopyAnnotationsCmd(UUID videoReferenceUuid,
                               VideoIndex videoIndex,
@@ -130,6 +130,6 @@ public class CopyAnnotationsCmd implements Command {
 
     @Override
     public String getDescription() {
-        return null;
+        return "Copy " copiedAnnotations.size() + " annotations";
     }
 }
