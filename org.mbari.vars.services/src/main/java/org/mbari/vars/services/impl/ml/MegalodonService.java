309ee892e31577c489c46b33b098de333474120b
Brian Schlining <bschlining@gmail.com>
2022-11-14 13:18:10 -0800

    check point. Having issues with proxy interfering with HTTP request to ML server

--- org.mbari.vars.services/src/main/java/org/mbari/vars/services/impl/ml/MegalodonService.java
+++ org.mbari.vars.services/src/main/java/org/mbari/vars/services/impl/ml/MegalodonService.java
@@ -4,25 +4,34 @@ import com.github.mizosoft.methanol.*;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.mbari.vars.core.util.StringUtils;
 import org.mbari.vars.services.MachineLearningService;
+import org.mbari.vars.services.methanol.LoggingInterceptor;
 import org.mbari.vars.services.model.MachineLearningLocalization;
-import org.mbari.vars.services.util.ImageUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
+
 import java.io.IOException;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.List;
 import java.util.UUID;
 
 /**
  * Implementation for https://github.com/mbari-org/keras-model-server-fast-api
+ * Test with httpie:
+ *
+ * <pre>
+ *     <code>
+ * http -f POST http://prometheus.shore.mbari.org:8082/predictor/ \
+ *   model_type=image_queue_yolov5 \
+ *   file@'/Users/brian/Downloads/testimages/20220302T214640Z--77cccf9c-691b-4eb4-bd74-49c290889dae.jpeg'
+ *     </code>
+ * </pre>
  */
 public class MegalodonService implements MachineLearningService {
 
@@ -36,7 +45,12 @@ public class MegalodonService implements MachineLearningService {
 
     public MegalodonService(String endpoint) {
         this.endpoint = endpoint;
-        this.client = Methanol.create();
+        this.client = Methanol
+                .newBuilder()
+                .connectTimeout(Duration.ofSeconds(30))
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .interceptor(new LoggingInterceptor())
+                .build();
     }
 
     @Override
@@ -44,8 +58,7 @@ public class MegalodonService implements MachineLearningService {
         var mediaType = image.endsWith(".png") ? MediaType.IMAGE_PNG : MediaType.IMAGE_JPEG;
 
         try {
-            var multipartBody = MultipartBodyPublisher.newBuilder()
-                    .textPart("model_type", "image_queue_yolov5", StandardCharsets.UTF_8)
+            var multipartBody = MultipartBodyPublisher.newBuilder(.textPart("model_type", "image_queue_yolov5", StandardCharsets.UTF_8)
                     .filePart("file", image, mediaType)
                     .build();
             var request = MutableRequest.POST(endpoint, multipartBody)
@@ -66,7 +79,7 @@ public class MegalodonService implements MachineLearningService {
     public List<MachineLearningLocalization> predict(byte[] jpegBytes) {
 
         try {
-            log.atDebug().log("Using byte[" + jpegBytes.length + "] for prediction");
+            log.atDebug().log("The JPEG image being send for prediction is " + jpegBytes.length + " bytes long");
 
             var imagePart = MoreBodyPublishers.ofMediaType(
                     HttpRequest.BodyPublishers.ofByteArray(jpegBytes), MediaType.IMAGE_JPEG);
